9484aaecaab214c526cf57dff080542fe0f2c655
Mohd Bilal Sheikh <70107735+MBS11@users.noreply.github.com>
2021-06-02 17:30:31 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
- 
 console.log("Welcome to notes app!");
 showNotes();
 
@@ -6,15 +5,21 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
 
   showNotes();
 });
@@ -32,8 +37,8 @@ function showNotes() {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-dark">Delete Note</button>
                     </div>
                 </div>`;
@@ -67,7 +72,7 @@ let search = document.getElementById('searchTxt');
 search.addEventListener("input", function(){
 
     let inputVal = search.value.toLowerCase();
-    
+   
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function(element){
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
@@ -77,6 +82,7 @@ search.addEventListener("input", function(){
         else{
             element.style.display = "none";
         }
-        
+      
     })
 })
+
